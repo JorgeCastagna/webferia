069502b9391fdc6cc221ae5b87f97a9628ec0467
Jorge <castagnajoc@gmail.com>
2023-06-19 17:07:27 -0300

    actualizacion de directorios

---
+++ assets/script/index.js
@@ -0,0 +1,19 @@
+var barra = `
+<div>
+    <input type="checkbox">
+    <i class="fas fa-bars"></i>
+    <i class="fas fa-times"></i>
+        <nav>
+            <ul>
+                <li><a href="/index.html">Inicio </a></li>
+                <li><a href="/assets/pantallas/productos.html">Productos</a></li>
+                <li><a href="/assets/pantallas/tienda.html">Tienda</a></li>
+                <li><a href="/assets/pantallas/registrarse.html">Registrarse</a></li>
+                <li><a href="/assets/pantallas/contacto.html">Contacto</a></li>
+                <li><a href="/assets/pantallas/sobrenosotros.html">Sobre nosotros</a></li>
+                <!--<li><a href="/assets/pantallas/carrito_de_compras.html">Carrito de compras</a></li>-->
+            </ul>
+        </nav>
+</div>`
+
+document.getElementById("navBarId").innerHTML = barra;
---
+++ assets/script/tienda.js
@@ -0,0 +1,44 @@
+
+fetch('https://sheetdb.io/api/v1/v8sid17znpqza')
+  .then(response => response.json()) // Parsear la respuesta como JSON
+  .then(data => {
+    // Aquí puedes trabajar con los datos obtenidos de la API
+    console.log(data); // Verifica que los datos se estén obteniendo correctamente
+
+    // Crear instancias de la clase Product y mostrar los datos en el HTML
+    var productObjeto = data.map(
+      (producto) => new Product(producto.articulo, producto.precio, producto.img),
+    );
+
+    productObjeto.forEach((art) => art.getDivCardProduct("grid-container"));
+  })
+  .catch(error => {
+    // Manejo de errores en caso de que la petición falle
+    console.error('Error:', error);
+  });
+
+class Product {
+  constructor(articulo, precio, img) {
+    this.articulo = articulo;
+    this.precio = precio;
+    this.img = img;
+  }
+
+  getDivCardProduct(idToInsert) {
+    let parent = document.getElementById(idToInsert);
+    let div = document.createElement("div");
+    div.innerHTML = `
+      <center>
+        <h4>${this.articulo}</h4>
+        <div>
+          <img src="${this.img}" alt="${this.name}" >
+          <h5>${this.precio}</h5>
+        </div>
+      </center>`;
+    parent.appendChild(div);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+  // El código dentro de esta función se ejecutará cuando se cargue el DOM
+});
