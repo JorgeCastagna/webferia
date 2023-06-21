4835648ab66c26baca7c76ff60f70cba004339ea
BJavierG <bjaviergg@gmail.com>
2023-06-21 00:41:30 -0300

    Se realizo correcciones de documentacion

--- assets/script/tienda.js
+++ assets/script/tienda.js
@@ -1,22 +1,3 @@
-
-fetch('https://sheetdb.io/api/v1/v8sid17znpqza')
-  .then(response => response.json()) // Parsear la respuesta como JSON
-  .then(data => {
-    // Aca se puede trabajar con los datos obtenidos de la API
-    console.log(data); // Verifica que los datos se estén obteniendo correctamente
-
-    // Crear instancias de la clase Product y mostrar los datos en el HTML
-    var productObjeto = data.map(
-      (producto) => new Product(producto.articulo, producto.precio, producto.img),
-    );
-
-    productObjeto.forEach((art) => art.getDivCardProduct("grid-container"));
-  })
-  .catch(error => {
-    // Manejo de errores en caso de que la petición falle
-    console.error('Error:', error);
-  });
-
 class Product {
   constructor(articulo, precio, img) {
     this.articulo = articulo;               
@@ -24,7 +5,7 @@ class Product {
     this.img = img;
   }
 
-  getDivCardProduct(idToInsert) {       //Estructuras de las tarjetas para la Tienda
+  getDivCardProduct(idToInsert) {       
     let parent = document.getElementById(idToInsert);   
     let div = document.createElement("div");            
     div.innerHTML = `
@@ -41,4 +22,21 @@ class Product {
 
 document.addEventListener("DOMContentLoaded", function() {
   // El código dentro de esta función se ejecutará cuando se cargue el DOM(Document Object Model)
+  fetch('https://sheetdb.io/api/v1/v8sid17znpqza')
+  .then(response => response.json()) // Parsear la respuesta como JSON
+  .then(data => {
+    // Aca se puede trabajar con los datos obtenidos de la API
+    console.log(data); // Verifica que los datos se estén obteniendo correctamente
+
+    // Crear instancias de la clase Product y mostrar los datos en el HTML
+    var productObjeto = data.map(
+      (producto) => new Product(producto.articulo, producto.precio, producto.img),
+    );
+
+    productObjeto.forEach((art) => art.getDivCardProduct("grid-container"));
+  })
+  .catch(error => {
+    // Manejo de errores en caso de que la petición falle
+    console.error('Error:', error);
+  });
 });
